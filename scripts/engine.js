3d0ecc9482f5e87f88d4d25af70236a12746c899
Emersonfernando <144748632+Emersonfernando@users.noreply.github.com>
2023-12-17 03:26:55 -0300

    Create engine.js

---
+++ scripts/engine.js
@@ -0,0 +1,63 @@
+const emojis =[
+    "⚽",
+    "⚽" ,
+    "😜",
+    "😜",
+    "✔",
+    "✔",
+    "😶",
+    "😶",
+    "😴",
+    "😴",
+    "😡",
+    "😡",
+];
+let openCards =[];
+
+
+let shuffleEmojis = emojis.
+sort(()=>(Math.random() > 0.5 ? 2 : -1));
+
+
+for(let i=0; i < emojis.length; i++)
+{
+    let box = document.createElement
+    ("div")
+    box.className = "item ";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleCLick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleCLick(){
+    if(openCards.lenght <2){
+        this.classList.add( "boxOpen");
+        openCards.push(this);
+    }
+    if(openCards.length ==2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+
+    if(openCards[0].innerHTML ===
+        openCards[1].innerHTML){
+            openCards[0].classList.add
+            ("boxMatch");
+            openCards[1].classList.add
+            ("boxMatch");
+        }else{
+            openCards[0].classList.remove
+            ("boxOpen");
+            openCards[1].classList.remove
+            ("boxOpen");
+        }
+
+        openCards = [];
+
+        if(document.querySelectorAll(".boxMatch").length === emojis.lenght){
+            alert("Você venceu!");
+        }
+    
+}
